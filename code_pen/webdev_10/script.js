c1464a0cf36a983675894caf7a9db1f0ca1e13d8
AshCUB <asre8419@colorado.edu>
2025-03-25 14:59:50 -0600

    ddd

--- code_pen/webdev_10/script.js
+++ code_pen/webdev_10/script.js
@@ -1,47 +1,58 @@
-gsap.config({trialWarn: false});
-let select = s => document.querySelector(s),
-		toArray = s => gsap.utils.toArray(s),
-		mainSVG = select('#mainSVG'),
-		allLats = toArray('.lat'),
-		startDuration = 2
-
-gsap.set('svg', {
-	visibility: 'visible'
-})
-let mainTl = gsap.timeline({
-	defaults: {		
-	ease: 'power3.inOut'
-	}
+const cursor = document.querySelector(".cursor");
+const button = document.querySelector("button");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
 });
-mainTl.fromTo('#outline', {
-	drawSVG: '75% 75%'
-},{
-	drawSVG: '-25% 75%',
-	duration: startDuration
-})
-.add('lines')
-.fromTo('.mid', {
-	drawSVG: (c) => c % 2 ? '100% 100%' : '0% 0%'
-},{
-	drawSVG: (c) => c % 2 ? '100% 50%' : '0% 50%',
-	duration: startDuration,
-	
-}, 'lines')
-.from('.outer', {
-	duration: startDuration,
-	drawSVG: (c) => c % 2 ? '100% 100%' : '0% 0%' 
-}, 'lines+=1')
-allLats.forEach((i, c) => {
-	let tl = gsap.timeline();
-	tl.set(i, {
-		strokeWidth: 4
-	})
-		.to(i, {
-		ease: 'none',
-		duration: 2,
-		repeat: -1
-	})
-	mainTl.add(tl, (startDuration*2+1) + (c/2.5))
-})
 
-//ScrubGSAPTimeline(tl)
+document.addEventListener("click", (e) => {
+  const svg = document.getElementById("cursorSvg");
+  const chunk = svg.querySelector(".chunk-pair:not(.falling)");
+  if (!chunk) return;
+
+  const newSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  newSvg.setAttribute("width", "50");
+  newSvg.setAttribute("height", "50");
+  newSvg.setAttribute("viewBox", "0 0 669 1012");
+  const clone = chunk.cloneNode(true);
+  newSvg.appendChild(clone);
+
+  newSvg.style.left = `${e.clientX}px`;
+  newSvg.style.top = `${e.clientY}px`;
+  newSvg.style.position = "fixed";
+  newSvg.style.transform = "translate(-50%, -50%)";
+
+  document.body.appendChild(newSvg);
+
+  chunk.remove();
+
+  const fallDistance = window.innerHeight - e.clientY + 50;
+  const xOffset = (Math.random() - 0.5) * 200;
+  const rotateDegree = (Math.random() - 0.5) * 90;
+
+  gsap.to(newSvg, {
+    duration: 1.2,
+    y: fallDistance,
+    x: xOffset,
+    rotation: rotateDegree,
+    opacity: 0,
+    ease: "power2.in",
+    onComplete: () => newSvg.remove()
+  });
+});
+
+let clickCount = 0;
+const texts = [
+  "Click here...",
+  "CLICK HERE",
+  "CLICK! HERE!",
+  "CLICK!!",
+  "CLICK!!!!!!!!!"
+];
+
+button.addEventListener("click", () => {
+  if (clickCount < texts.length) {
+    text.textContent = texts[clickCount];
+    clickCount++;
+  }
+});
